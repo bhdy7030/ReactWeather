0f847d31ce0d46cc640e7faff9c46da7bc23d396
bhdy7030 <yuandu.cs@gmail.com>
2017-04-25 20:53:34 -0400

    error handling

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -10,13 +10,21 @@ module.exports = {
     var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=?${encodingLocation}`;
 
     return axios.get(requestUrl).then(function(res){
+      // if(res.data.cod && res.data.cod==='200'){
+      //   return res.data.main.temp;
+      // }
+      // else{
+      //   throw new Error(res.data.message);
+      //}
+      debugger;
       if(res.data.cod && res.data.message){
         throw new Error(res.data.message);
       }else{
         return res.data.main.temp;
       }
     }, function(res){
-      throw new Error(res.data.message);
+        //debugger;
+      throw new Error(res.response.data.message);
     });
   }
 }
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,26 +1,9 @@
 var React = require('react');
 
-// var WeatherMessage = React.createClass({
-//   render: function(){
-//
-//     var {temp,location} = this.props;
-//
-//     return (
-//       <div>
-//         <h3>It's {temp} in {location} </h3>
-//       </div>
-//     );
-//   }
-// });
-
-var WeatherMessage = ({temp,location})=>{
-  // var {temp,location} = props;
-
+var WeatherMessage = ({temp, location}) => {
   return (
-    <div>
-      <h3 className="text-center">It's {temp} in {location} </h3>
-    </div>
-  );
+    <h3 className="text-center">It's it {temp} in {location}.</h3>
+  )
 };
 
 module.exports = WeatherMessage;
